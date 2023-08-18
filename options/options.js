1ddb819cb6817ab2e9fc567aa85932f4a882f66c
Crowdbotics <team@crowdbotics.com>
2023-08-18 13:14:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "easy-money-42932",
+  url: "https://easy-money-42932.botics.co",
+  api: "https://easy-money-42932.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
